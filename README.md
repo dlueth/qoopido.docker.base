fbdc075e2e49a550c0c1c116b12527998e6a729a
Dirk Lüth <dlueth@Dirks-MacBook-Pro.local>
2016-10-20 12:26:55 +0200

    [UPDATE] add files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is just a base for other qoopido/* containers ;)
---
+++ config/my_init.d/00_initialize.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+up() {
+    local UP="/app/config/up.sh"
+
+    if [ -f $UP ]
+    then
+        echo "    Running startup script /app/config/up.sh"
+        chmod +x $UP && chmod 755 $UP && eval $UP;
+    fi
+}
+
+down()
+{
+	local DOWN="/app/config/down.sh"
+
+	if [ -f $DOWN ]
+    then
+    	echo "    Running shutdown script /app/config/down.sh"
+		chmod +x $DOWN && chmod 755 $DOWN && eval $DOWN;
+    fi
+
+    exit
+}
+
+trap down SIGTERM
+up
